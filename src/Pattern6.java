34c5e5129789f125d33664c1314346a7e52c7454
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-15 16:15:22 +0530

    half pyramid with number pattern program

--- src/Pattern6.java
+++ src/Pattern6.java
@@ -10,7 +10,7 @@ public class Pattern6 {
          {
              for(int j=1;j<=i;j++)
              {
-                 System.out.print(j);
+                 System.out.print(j+" ");
              }
              System.out.println();
          }
