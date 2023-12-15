617a7162d47a09041610205841a15ab530fd0864
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-15 14:11:59 +0530

    inverted half pyramid pattern program

--- src/Pattern4.java
+++ src/Pattern4.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Pattern4 {
+import java.util.Scanner;
+
+//inverted half pyramid
+public class Pattern4 {
+    public static void main(String[] args) {
+        int n,i,j;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter no. of row");
+        n=sc.nextInt();
+        for (i=n;i>=1;i--)
+        {
+            for (j=1;j<=i;j++)
+
+            {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
