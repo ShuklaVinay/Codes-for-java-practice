5e8096d07a759a708252730c5a377f340cf248f6
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-15 15:28:42 +0530

    inverted half pyramid(roted by 180 degree) pattern program

---
+++ src/pattern5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//inverted half pyramid rotated by 180 degree
+public class pattern5 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter no. of rows:");
+        n=sc.nextInt();
+        for (int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
