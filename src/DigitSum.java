e0e128fa9fd98c6d7df01e3a2eb991998ea1eafb
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-16 16:24:47 +0530

    enter no. and find sum of digits

---
+++ src/DigitSum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DigitSum {
+    public static void main(String[] args) {
+        int n,r,sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter the no. for sum:");
+        n=sc.nextInt();
+        while(n>0)
+        {
+            r=n%10;
+            sum=sum+r;
+            n=n/10;
+        }
+        System.out.println("sum of entered digit ="+ " "+sum);
+    }
+}
