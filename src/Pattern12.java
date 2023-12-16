ab6679b3c2d45ca3462becb1d7965eba0f430d91
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-16 15:32:34 +0530

    pattern program no. 12 in method

--- src/Pattern12.java
+++ src/Pattern12.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
 
 public class Pattern12 {
-    public static void main(String[] args) {
-        int n, m;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the value of n & m:");
-        n=sc.nextInt();
-        m= sc.nextInt();
-       for(int i=1;i<=n;i++)
-       {
-           for (int j=1;j<=m;j++)
-           {
+    public static void pattern(int n,int m)
+    {
+
+        for(int i=1;i<=n;i++)
+        {
+            for (int j=1;j<=m;j++)
+            {
                 if (i==1 || i==n || j==1 || j==m) {
                     System.out.print("*");
                 }
@@ -18,10 +15,19 @@ public class Pattern12 {
                     System.out.print("#");
                 }
 
-           }
+            }
 
-               System.out.println();
-           }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        int n, m;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of n & m:");
+        n=sc.nextInt();
+        m= sc.nextInt();
+        pattern(n,m);
        }
     }
 
