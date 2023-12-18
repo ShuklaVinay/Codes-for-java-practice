b27249a1803ce148716702fde816c5dc91d5a433
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-18 14:33:31 +0530

    //palindromic number pyramid different typepattern program

---
+++ src/Pattern17.java
@@ -0,0 +1,25 @@
+//palindromic number pyramid different type
+
+import java.util.Scanner;
+
+
+public class Pattern17 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter value of n:");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+}
