8e260e1364922ef719e0ab2cbd0a87dcc3dfb8a6
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-18 13:32:16 +0530

    Number pyramid pattern program

---
+++ src/Pattaern14.java
@@ -0,0 +1,22 @@
+// Number pyramid pattern program
+import java.util.Scanner;
+public class Pattaern14 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter value of n for pattern");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++)
+            {
+                System.out.print(i+" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
