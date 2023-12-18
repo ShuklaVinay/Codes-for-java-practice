453d975fc46c60c17ad10da77e104b54dc741cb5
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-18 14:17:51 +0530

    palindromic number pyramid pattern program

---
+++ src/Pattern16.java
@@ -0,0 +1,25 @@
+//palindromic number pyramid pattern program
+import java.util.Scanner;
+public class Pattern16 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter the value of n");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+           for(int j=1;j<=n-i;j++)
+           {
+               System.out.print(" ");
+           }
+            for (int j=i;j>=1;j--)
+            {
+                System.out.print(j);
+            }
+            for (int j=2;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
