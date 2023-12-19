3a311ab2ff214ce74b3854eab4622ecf1000cb24
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-19 23:54:00 +0530

    damaru pattern program

---
+++ src/Pattern21.java
@@ -0,0 +1,38 @@
+//print damaru pattern
+import java.util.Scanner;
+public class Pattern21 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter the value of n");
+        n=sc.nextInt();
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=2*i-1;j++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<n-i;j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=2*i+1;j++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
