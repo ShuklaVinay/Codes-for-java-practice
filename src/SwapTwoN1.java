00f0258d930278b23c529b147ee43e0c80d264ae
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-22 00:07:38 +0530

    Swap two number by using division & multiplication

---
+++ src/SwapTwoN1.java
@@ -0,0 +1,27 @@
+//Swap two number by using division & multiplication
+import java.util.Scanner;
+public class SwapTwoN1 {
+    public static void swap(int a,int b)
+    {
+
+        System.out.println("original value before swapping :");
+        System.out.println("a="+" "+a+" "+"b="+" "+b);
+        a=a*b;
+        b=a/b;
+        a=a/b;
+        System.out.println("Swapped  value by using bitwise operator(XOR ^) :");
+        System.out.println("a="+" "+a+" "+"b="+" "+b);
+    }
+
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first number:");
+        a=sc.nextInt();
+        System.out.println("enter the second number:");
+        b=sc.nextInt();
+        swap(a,b);
+
+    }
+}
+
