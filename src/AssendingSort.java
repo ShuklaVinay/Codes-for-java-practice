5c1427eb0135a986bb7562b9f2b401fbd3262298
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-21 20:46:28 +0530

    Sorting Array Ascending order

---
+++ src/AssendingSort.java
@@ -0,0 +1,29 @@
+//Sorting Array Ascending order
+
+public class AssendingSort {
+
+    static int[] customSort(int a[]) {
+        int temp = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{78, 34, 1, 3, 90, 34, -1, -4, 6, 55, 20, -7777, -99, 65};
+
+        int[] newArr = customSort(a);
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
+}
