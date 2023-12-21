f573b92e4ff7452a823804fce00f1e6a4f67cf50
Vinay <vinay@Vinays-MacBook-Pro.local>
2023-12-22 00:17:49 +0530

    swap two value by using 3rd variable

--- src/SwapTwoN2.java
+++ src/SwapTwoN2.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class SwapTwoN2 {
+//swap two value by using 3rd variable
+import java.util.Scanner;
+
+public class SwapTwoN2 {
+    public static void swap(int a,int b)
+    {
+        int c;
+        System.out.println("original value before swapping :");
+        System.out.println("a="+" "+a+" "+"b="+" "+b);
+        c=a;
+        a=b;
+        b=c;
+        System.out.println("Swapped  value by using 3rd variable :");
+        System.out.println("a="+" "+a+" "+"b="+" "+b);
+    }
+
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the first number:");
+        a=sc.nextInt();
+        System.out.println("enter the second number:");
+        b=sc.nextInt();
+        swap(a,b);
+
+    }
 }
